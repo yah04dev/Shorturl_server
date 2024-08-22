87991045f892347ebcd766cd571c0117bcbc72a3
yah04dev <46074913+yah04dev@users.noreply.github.com>
2024-08-22 23:03:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import redirect,url_for,Flask, request,render_template,make_response
 import sqlite3
+from datetime import datetime
+
 app = Flask(__name__)
 connection = sqlite3.connect('links.db', check_same_thread=False)
 cursor = connection.cursor()
@@ -9,13 +11,24 @@ def index():
    return render_template('index.html')
 @app.route('/create')
 def create():
-   link = request.args.get('link')
-   cursor.execute('''INSERT INTO links (rlink, LV,owi,VT) VALUES (?, ?,?,?)''', (link, current_time,0,"0"))
-   com="SELECT id FROM links WHERE rlink = "+link
-   id = cursor.fetchall()
-   id=list(id[0])[0]
-   return render_template('created.html',id=id)
-   
+    link = request.args.get('link')
+    if link is None:
+        return "Error: No link provided", 400
+    
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    cursor = connection.cursor()
+    cursor.execute('''INSERT INTO links (rlink, LV, owi, VT) VALUES (?, ?, ?, ?)''', 
+                   (link, current_time, 0, "0"))
+    connection.commit() 
+
+    
+    cursor.execute('SELECT id FROM links WHERE rlink = ?', (link,))
+    id = cursor.fetchall()
+    id = list(id.pop())[0]
+    print(id)
+    return render_template('created.html',id=id)
+    connection.close()   
 
 @app.route('/r/<x>')
 def redirection(x): 
@@ -24,5 +37,5 @@ def redirection(x):
     link = cursor.fetchall()
     link=list(link[0])[0]
     return redirect(link)
-  
-app.run()
+    connection.close()
+app.run(debug=True)
